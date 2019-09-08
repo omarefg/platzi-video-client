518c3887a94f5062e53917c3a7439f58da8d9dde
Omar Flores Grimontt <omar.flores@3dves.com>
2019-09-08 11:19:09 -0500

    prepared work space

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { container } from './assets/styles/App.module.scss';
+
+const App = (props) => {
+  return (
+    <h1 className={container}>Hello bro</h1>
+  );
+};
+
+export default App;
